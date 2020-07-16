9ec8b6120bd22833160f4da53245bfecc754e78b
it-green <ryo.ctuad@gmail.com>
2020-07-16 11:58:24 +0900

    [add] 画像圧縮処理の追加

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const ejs = require('gulp-ejs');
 const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssDeclarationSorter = require('css-declaration-sorter');
+const imgMin = require('gulp-imagemin');
+// const imgMinPng = require('imagemin-pngquant');
+// const imgMinJpg = require('imagemin-jpeg-recompress');
+// const imgMinGif = require('imagemin-gifsicle');
+// const svgMin = require('gulp-svgmin');
 const browserSync = require('browser-sync').create();
 
 function html() {
@@ -13,8 +21,17 @@ function html() {
 }
 
 function css() {
+    const plugin = [
+        autoprefixer(),
+        cssDeclarationSorter({
+            order: 'smacss'
+        })
+    ];
     return src('./dev/scss/*.scss')
-    .pipe(sass())
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(postcss(plugin))
     .pipe(dest('./dist/css'));
 }
 
@@ -26,13 +43,27 @@ function js() {
     .pipe(dest('./dist/js'))
 }
 
+function img() {
+    return src('./dev/img/*')
+    .pipe(imgMin())
+    .pipe(dest('./dist/img'))
+}
+
+// function svg() {
+//     return src('./dev/img/*.svg')
+//     .pipe(svgMin())
+//     .pipe(dest('./dist/img'))
+// }
+
 function watcher() {
     watch('./dev/pages/**/*.ejs', html);
     watch('./dev/scss/*.scss', css);
     watch('./dev/js/*.js', js);
+    watch('./dev/img', img);
     watch('./**/*.html', reload);
     watch('./dist/css/*.css', reload);
     watch('./dist/js/*.js', reload);
+    watch('./dist/img', reload);
 }
 
 function serve(done) {
@@ -54,6 +85,8 @@ function reload(done) {
 exports.ejs = html;
 exports.css = css;
 exports.js = js;
+exports.img = img;
+// exports.svg = svg;
 exports.serve = serve;
 exports.watcher = watcher;
-exports.default = parallel(html, css, js, serve, watcher);
+exports.default = parallel(img, html, css, js, serve, watcher);
